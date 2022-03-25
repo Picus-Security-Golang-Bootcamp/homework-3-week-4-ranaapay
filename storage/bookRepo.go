d47e883b8c6ee1b91dced9638903948a9c122c54
ranaapay <ranapay.itu@gmail.com>
2022-03-26 02:50:08 +0300

    GetById, FindByName added.

--- storage/bookRepo.go
+++ storage/bookRepo.go
@@ -3,7 +3,7 @@ package storage
 import (
 	"PicusHomework3/src/storage/helper"
 	"PicusHomework3/src/storage/storageType"
-	"fmt"
+	"errors"
 	"gorm.io/gorm"
 )
 
@@ -15,7 +15,21 @@ func NewBookRepository(db *gorm.DB) *BookRepository {
 	return &BookRepository{db: db}
 }
 
+func (b *BookRepository) GetById(id int) (*storageType.Book, error) {
+	var book storageType.Book
+	result := b.db.First(&book, id)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil, result.Error
+	}
+	return &book, nil
+}
 
+func (b *BookRepository) FindByName(name string) []storageType.Book {
+	var books []storageType.Book
+	b.db.Where("Name LIKE ? ", "%"+name+"%").
+		Find(&books)
+	return books
+}
 
 func (b *BookRepository) Migrations() {
 	b.db.AutoMigrate(&storageType.Book{})
@@ -27,7 +41,6 @@ func (b *BookRepository) InsertSampleData() error {
 	if err != nil {
 		return err
 	}
-	fmt.Println(books[1])
 	b.db.Create(&books)
 	return nil
 }
